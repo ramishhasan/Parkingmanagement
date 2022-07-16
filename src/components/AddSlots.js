48e679c479f42d46d98a620833cf22d742fe7f24
Ramish <ramishhasan012@gmail.com>
2022-07-16 09:54:54 +0530

    First

---
+++ src/components/AddSlots.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class AddSlots extends Component {
+  state = {};
+
+  // onClick = (e) =>{
+  //     this.preventDefault();
+  //     this.props.AddSlots()
+  // }
+  render() {
+    return (
+      <div>
+        <h2>Welcome To Parking Area</h2>
+      </div>
+    );
+  }
+}
+
+export default AddSlots;
